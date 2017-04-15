397a00114874e031fde54a44fae0c8dd608b0f88
Roopansh Bansal <roopansh@iitg.ernet.in>
2017-04-15 19:55:39 +0530

    test

--- app.py
+++ app.py
@@ -4,13 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    # the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-    return jsonify({"success": "true"})
-    # return """
-    # <h1>Hello heroku</h1>
-    # <p>It is currently {time}.</p>
-    # <img src="http://loremflickr.com/600/400" />
-    # """.format(time=the_time)
-
+	return jsonify({"success": "true"})
+	
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+	app.run(debug=True, use_reloader=True)
