5155b85cc01119f98d797ec7edd9bbd04894a0cf
Roopansh Bansal <roopansh@iitg.ernet.in>
2017-04-15 19:50:37 +0530

    hello

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    <img src="http://loremflickr.com/600/400" />
+    return jsonify({"success": "true"})
+    # return """
+    # <h1>Hello heroku</h1>
+    # <p>It is currently {time}.</p>
+    # <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
 
 if __name__ == '__main__':
